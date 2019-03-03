11600c07fe7cda8a8a6a371c2b7f2a45c595e14d
SaberEDM <48182428+SaberEDM@users.noreply.github.com>
2019-03-04 01:10:35 +0700

    Add files via upload

---
+++ src/democi/DemoCI.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package democi;
+
+import calculation.Add;
+import calculation.Multi;
+import java.util.Scanner;
+
+/**
+ *
+ * @author King Arthur
+ */
+public class DemoCI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("***** Demo CI With Java *****");
+        System.out.println("***** Compare Two Number *****");
+        System.out.println("");
+        
+        Add a = new Add();
+        int result1 = a.Add(1, 2);
+        System.out.println(result1);
+        
+        Multi m = new Multi();
+        int result2 = m.Multi(2, 5);
+        System.out.println(result2);
+        
+    }
+
+}
