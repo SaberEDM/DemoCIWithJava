36af13ba1cf3fa772772b0f26547a53976477ab4
SaberEDM <48182428+SaberEDM@users.noreply.github.com>
2019-03-04 01:11:21 +0700

    Add files via upload

---
+++ src/calculation/Multi.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculation;
+
+/**
+ *
+ * @author King Arthur
+ */
+public class Multi {
+    public int Multi(int number1, int number2) {
+        return number1 * number2;
+    }
+}
