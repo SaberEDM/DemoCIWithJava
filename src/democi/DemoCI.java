797eeb8b826ea79b373487dafab94524ad0516a3
SaberEDM <48182428+SaberEDM@users.noreply.github.com>
2019-03-04 11:41:49 +0700

    Update DemoCI.java

--- src/democi/DemoCI.java
+++ src/democi/DemoCI.java
@@ -23,7 +23,7 @@ public class DemoCI {
         System.out.println("");
         
         Add a = new Add();
-        int result1 = a.Add(1, y);
+        int result1 = a.Add(1, 2);
         System.out.println(result1);
         
         Multi m = new Multi();
